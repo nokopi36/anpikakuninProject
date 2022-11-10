e0e78ee8570cf1b9a7c915bea4ef4db338affc5b
RyotaHiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2022-11-10 15:20:33 +0900

    バックグラウンドでのプッシュ通知機能の実装

--- app/src/main/java/com/hiyama/anpikakuninproject/LoginActivity.kt
+++ app/src/main/java/com/hiyama/anpikakuninproject/LoginActivity.kt
@@ -3,17 +3,25 @@ package com.hiyama.anpikakuninproject
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.lifecycleScope
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.google.android.gms.tasks.OnCompleteListener
 import kotlinx.coroutines.launch
+import com.google.firebase.messaging.FirebaseMessaging
 
 class LoginActivity : AppCompatActivity() {
 
     val commServer = CommServer()
+
+//    val builder = NotificationCompat.Builder(this, )
 
 
 
@@ -31,7 +39,37 @@ class LoginActivity : AppCompatActivity() {
         testBtn.setOnClickListener {
             commServer.setURL(CommServer.TEST)
             receiveInfo()
+            val postData = jacksonObjectMapper().writeValueAsString(PostTest.getPostData())
+            Log.i("postData",postData )
         }
+
+        val postBtn = findViewById<Button>(R.id.postBtn)
+        postBtn.setOnClickListener {
+            commServer.setURL(CommServer.POSTTEST)
+            receiveInfo2()
+        }
+
+        // インスタンスIDの自動生成を有効化する場合、true
+        // AndroidManifestにて自動生成を禁止にしていない場合、不要
+//        FirebaseMessaging.getInstance().isAutoInitEnabled = true
+
+        // Current Notificationトークンの取得
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    // token取得失敗
+                    Log.d("getInstanceId failed ${task.exception}", "a")
+                    return@OnCompleteListener
+                }
+
+                // new Instance ID token
+                // ここで取得したtokenをテストする際のインスタンスIDとして設定する
+                val token = task.result
+
+                val msg = "InstanceID Token: $token"
+                Log.d("msg",msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            })
 
     }
 
@@ -40,6 +78,17 @@ class LoginActivity : AppCompatActivity() {
         val testTxt = findViewById<TextView>(R.id.testText)
         lifecycleScope.launch {
             val result = commServer.getInfoBackGroundRunner("UTF-8")
+            Log.i("GET",result)
+            testTxt.text = result
+        }
+    }
+
+    @UiThread
+    private fun receiveInfo2(){ //posttest
+        val testTxt = findViewById<TextView>(R.id.postText)
+        lifecycleScope.launch {
+            val result = commServer.getInfoBackGroundRunner("UTF-8")
+            Log.i("POST",result)
             testTxt.text = result
         }
     }
---
+++ app/src/main/java/com/hiyama/anpikakuninproject/MyFirebaseMessagingService.kt
@@ -0,0 +1,70 @@
+package com.hiyama.anpikakuninproject
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService :FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        Log.i("Refreshed token: $token", "a")
+
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String?) {
+        // tokenをserverへ送る
+        Log.i("sendRegistrationTokenToServer($token)", "a")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.i("From: ${remoteMessage.from}", "a")
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.body?.let { body ->
+            Log.i("Message Notification Body:$body", "a")
+            sendNotification(body)
+        }
+    }
+
+    // notificationを生成して表示
+    private fun sendNotification(messageBody: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_IMMUTABLE)
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("FCM Message")
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+}
