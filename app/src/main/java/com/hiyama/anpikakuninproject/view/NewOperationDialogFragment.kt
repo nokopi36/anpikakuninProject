5d8767faf73352206eba0fe10b0779e7282f1d7e
RyotaHiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2022-12-15 14:30:28 +0900

    update:ボタン追加機能修正

--- app/src/main/java/com/hiyama/anpikakuninproject/view/NewOperationDialogFragment.kt
+++ app/src/main/java/com/hiyama/anpikakuninproject/view/NewOperationDialogFragment.kt
@@ -4,11 +4,14 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
+import android.webkit.URLUtil
 import android.widget.EditText
 import androidx.fragment.app.DialogFragment
 import com.hiyama.anpikakuninproject.R
 import com.hiyama.anpikakuninproject.data.OperationInfo
-import com.hiyama.anpikakuninproject.data.ScheduleInfo
+import java.net.MalformedURLException
+
 
 class NewOperationDialogFragment: DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -16,7 +19,7 @@ class NewOperationDialogFragment: DialogFragment() {
             val builder = AlertDialog.Builder(it)
 
             //Dialogレイアウトにviewを取得
-            val inflater = requireActivity().layoutInflater;
+            val inflater = requireActivity().layoutInflater
             val root = inflater.inflate(R.layout.newoperation_dialog, null)
 
             val buttonTitleEditText = root.findViewById<EditText>(R.id.buttonTitle)
@@ -28,8 +31,24 @@ class NewOperationDialogFragment: DialogFragment() {
                 .setPositiveButton("追加") { _, _ ->
                     val buttonTitle = buttonTitleEditText.text.toString()
                     val url = urlEditText.text.toString()
-                    OperationInfo.buttonTitle.add(buttonTitle)
-                    OperationInfo.url.add(url)
+                    if (buttonTitle.isEmpty() || url.isEmpty()){
+                        androidx.appcompat.app.AlertDialog.Builder(activity!!)
+                            .setTitle("●追加失敗")
+                            .setMessage("登録名もしくはURLが入力されていません")
+                            .setPositiveButton("OK") { _, _ -> }
+                            .show()
+                    } else {
+                        if (isValid(url)){
+                            OperationInfo.buttonTitle.add(buttonTitle)
+                            OperationInfo.url.add(url)
+                        } else {
+                            androidx.appcompat.app.AlertDialog.Builder(activity!!)
+                                .setTitle("●追加失敗")
+                                .setMessage("URLが無効です")
+                                .setPositiveButton("OK") { _, _ -> }
+                                .show()
+                        }
+                    }
                     Log.i(">>>", "Info.buttonTitle:${OperationInfo.buttonTitle}, Info.url:${OperationInfo.url}")
                 }
                 //キャンセルボタン
@@ -40,4 +59,12 @@ class NewOperationDialogFragment: DialogFragment() {
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
+
+    private fun isValid(urlString: String): Boolean {
+        try {
+            return URLUtil.isValidUrl(urlString) && Patterns.WEB_URL.matcher(urlString).matches()
+        } catch (_: MalformedURLException) { }
+        return false
+    }
+
 }
