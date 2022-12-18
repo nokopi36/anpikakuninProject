02b227a4ce6dc41368d9d1be4122b3d8dc8df149
RyotaHiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2022-12-18 15:59:49 +0900

    update:LinearLayoutのScrollViewへの変更

--- app/src/main/java/com/hiyama/anpikakuninproject/activity/OperationInfoFragment.kt
+++ app/src/main/java/com/hiyama/anpikakuninproject/activity/OperationInfoFragment.kt
@@ -3,22 +3,19 @@ package com.hiyama.anpikakuninproject.activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.text.Layout
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.ScrollView
 import androidx.annotation.UiThread
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import com.hiyama.anpikakuninproject.CommServer
 import com.hiyama.anpikakuninproject.R
 import com.hiyama.anpikakuninproject.data.OperationInfo
 import com.hiyama.anpikakuninproject.view.NewOperationDialogFragment
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class OperationInfoFragment : Fragment() {
