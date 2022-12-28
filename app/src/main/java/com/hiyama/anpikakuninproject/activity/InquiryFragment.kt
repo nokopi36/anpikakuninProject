a650e66b725dc2caaa0e7e9c6ea801cbc94b20b1
BuildTools <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2022-12-28 13:57:47 +0900

    add:問い合わせ機能追加

--- app/src/main/java/com/hiyama/anpikakuninproject/activity/InquiryFragment.kt
+++ app/src/main/java/com/hiyama/anpikakuninproject/activity/InquiryFragment.kt
@@ -1,27 +1,51 @@
 package com.hiyama.anpikakuninproject.activity
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.annotation.UiThread
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.hiyama.anpikakuninproject.CommServer
 import com.hiyama.anpikakuninproject.R
+import com.hiyama.anpikakuninproject.data.InquiryInfo
+import kotlinx.coroutines.runBlocking
 
 class InquiryFragment : Fragment() {
+
+    private val commServer = CommServer()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         val fragmentView = inflater.inflate(R.layout.fragment_inquiry, container, false)
 
-//        val mPushBtm = fragmentView.findViewById<Button>(R.id.pushBtn)
-//        val pushText = fragmentView.findViewById<TextView>(R.id.text)
-//        mPushBtm.setOnClickListener {
-//            pushText.text = "iiiii"
-//        }
+        val inquiryTitleEdit = fragmentView.findViewById<EditText>(R.id.inquiry_title)
+        val inquiryContentEdit = fragmentView.findViewById<EditText>(R.id.inquiry_content)
+
+        val sendBtn = fragmentView.findViewById<Button>(R.id.sendBtn)
+        sendBtn.setOnClickListener {
+            InquiryInfo.title = inquiryTitleEdit.text.toString()
+            InquiryInfo.content = inquiryContentEdit.text.toString()
+            postInfo()
+        }
 
         return  fragmentView
+    }
+
+    @UiThread
+    private fun postInfo(): String{
+        commServer.setURL(CommServer.INQUIRY)
+        var result: String
+        runBlocking { // postして結果が返ってくるまで待機
+            result = commServer.postInfoBackGroundRunner("UTF-8")
+            Log.i("POST",result)
+        }
+        return result
     }
 
 }
---
+++ app/src/main/java/com/hiyama/anpikakuninproject/data/InquiryInfo.kt
@@ -0,0 +1,6 @@
+package com.hiyama.anpikakuninproject.data
+
+object InquiryInfo {
+    var title = ""
+    var content = ""
+}
