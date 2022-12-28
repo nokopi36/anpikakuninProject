0a90b8f69bc3d9392724d372d1d022d6ca57ad65
BuildTools <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2022-12-28 14:31:22 +0900

    update:お知らせの有無でtext変更

--- app/src/main/java/com/hiyama/anpikakuninproject/activity/NotificationsFragment.kt
+++ app/src/main/java/com/hiyama/anpikakuninproject/activity/NotificationsFragment.kt
@@ -27,14 +27,18 @@ import org.junit.Test
 class NotificationsFragment : Fragment() {
 
     val commServer = CommServer()
-    lateinit var notification: List<Notification>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         val fragmentView = inflater.inflate(R.layout.fragment_notifications, container, false)
         val addLinearLayout = fragmentView.findViewById<LinearLayout>(R.id.addLinearLayout)
+        val nothingNotification = fragmentView.findViewById<TextView>(R.id.nothingNotification)
 
-        getNotification(addLinearLayout)
+        if (getNotification(addLinearLayout)){
+            nothingNotification.text = ""
+        } else {
+            nothingNotification.text = "お知らせはありません"
+        }
 
         val updateBtn = fragmentView.findViewById<Button>(R.id.updateBtn)
             updateBtn.setOnClickListener {
@@ -65,22 +69,22 @@ class NotificationsFragment : Fragment() {
 ////        }
 //    }
 
-    private fun getNotification(linearLayout: LinearLayout){
+    private fun getNotification(linearLayout: LinearLayout): Boolean{
         val result = getInfo()
 //        while(commServer.responseCode == -1){/* wait for response */}
         Log.i("Return Val From Server", "Value: $result")
         val notification = JsonParser.newsParse(result)
-        Log.i("notification parser", notification!![0].toString())
-        Log.i("ccccc", notification[0].news_id.toString())
-        Log.i("aaaaa", notification[0].title!!)
-        Log.i("bbbbb", notification[0].content!!)
-        Log.i("ccccc", notification[1].news_id.toString())
-        Log.i("aaaaa", notification[1].title!!)
-        Log.i("bbbbb", notification[1].content!!)
         linearLayout.removeAllViews()
-        if (notification.isEmpty()){
-            /* do nothing */
+        return if (notification == null){
+            false
         } else {
+            Log.i("notification parser", notification[0].toString())
+            Log.i("ccccc", notification[0].news_id.toString())
+            Log.i("aaaaa", notification[0].title!!)
+            Log.i("bbbbb", notification[0].content!!)
+            Log.i("ccccc", notification[1].news_id.toString())
+            Log.i("aaaaa", notification[1].title!!)
+            Log.i("bbbbb", notification[1].content!!)
             for ((index, _) in notification.withIndex()){
                 val button = Button(context)
                 button.text = notification[index].title
@@ -89,11 +93,13 @@ class NotificationsFragment : Fragment() {
                 }
                 linearLayout.addView(button)
             }
+            true
         }
     }
 
     @UiThread
     fun getInfo(): String{
+//        val result = ""
         val result = "{\"news\":[{\"news_id\": 2, \"title\": \"1\", \"content\": \"news1\"}, {\"news_id\": 1, \"title\": \"2\", \"content\": \"news2\"}]}"
         Log.i("test result", result)
 //        commServer.setURL(CommServer.NOTIFICATION)
