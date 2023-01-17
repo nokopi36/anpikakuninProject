e1c2375a604dc971ac0ae7026dadbb479d4cfa90
RyotaHiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2023-01-17 17:47:02 +0900

    update：変数名を変更、newsを取得可能に

--- app/src/main/java/com/hiyama/anpikakuninproject/activity/NotificationsFragment.kt
+++ app/src/main/java/com/hiyama/anpikakuninproject/activity/NotificationsFragment.kt
@@ -10,12 +10,12 @@ import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
-import androidx.annotation.UiThread
 import androidx.fragment.app.Fragment
 import com.hiyama.anpikakuninproject.utils.CommServer
 import com.hiyama.anpikakuninproject.R
 import com.hiyama.anpikakuninproject.data.*
 import com.hiyama.anpikakuninproject.utils.Safety
+import java.net.HttpURLConnection
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
@@ -62,66 +62,32 @@ class NotificationsFragment : Fragment() {
         return fragmentView
     }
 
-//    fun getNotification(){
-//        val result = getInfo()
-//        val notificationTitle = arrayListOf<String>()
-//        val notificationContext = arrayListOf<String>()
-////        while(commServer.responseCode == -1){/* wait for response */}
-////        if (commServer.responseCode == HttpURLConnection.HTTP_OK) {
-//            Log.i("Return Value From Server", "Value: $result")
-//            val notification = JsonParser.notificationParse(result)
-//        Log.i("notification parser", notification.toString())
-//            if (notification == null){
-//                Toast.makeText(activity, "予期せぬメッセージを受信しました", Toast.LENGTH_LONG).show()
-//            } else {
-////                NotificationInfo.initialize(notification)
-//                for (s in notification){
-//                    notificationTitle.add(s.title)
-//                    notificationContext.add(s.content)
-//                }
-//            }
-////        }
-//    }
-
     private fun getNotification(linearLayout: LinearLayout): Boolean{
-        val result = getInfo()
-//        while(commServer.responseCode == -1){/* wait for response */}
+        commServer.setURL(CommServer.NOTIFICATION)
+        val result = commServer.getInfo()
+//        val result = "{\"news\":[{\"auth_id\": 2, \"title\": \"1\", \"content\": \"news1\"}, {\"news_id\": 1, \"title\": \"2\", \"content\": \"news2\"}, {\"news_id\": 3, \"title\": \"3\", \"content\": \"news3\"}]}"
+        while(commServer.responseCode == -1){/* wait for response */}
         Log.i("Return Val From Server", "Value: $result")
         val notification = JsonParser.newsParse(result)
         linearLayout.removeAllViews()
-        return if (notification == null){
-            false
-        } else {
-//            Log.i("ccccc", notification[0].news_id.toString())
-//            Log.i("aaaaa", notification[0].title!!)
-//            Log.i("bbbbb", notification[0].content!!)
-//            Log.i("ccccc", notification[1].news_id.toString())
-//            Log.i("aaaaa", notification[1].title!!)
-//            Log.i("bbbbb", notification[1].content!!)
-            val sortedNotification = notification.sortedWith(compareByDescending{ it.news_id })
-            for ((index, _) in sortedNotification.withIndex()){
-                val button = Button(context)
-                button.text = sortedNotification[index].title
-                button.setOnClickListener {
-                    Toast.makeText(activity, sortedNotification[index].content, Toast.LENGTH_SHORT).show()
+        if (commServer.responseCode == HttpURLConnection.HTTP_OK){
+            return if (notification.isNullOrEmpty()){
+                false
+            } else {
+                val sortedNotification = notification.sortedWith(compareByDescending{ it.news_id })
+                for ((index, _) in sortedNotification.withIndex()){
+                    val button = Button(context)
+                    button.text = sortedNotification[index].title
+                    button.setOnClickListener {
+                        Toast.makeText(activity, sortedNotification[index].content, Toast.LENGTH_SHORT).show()
+                    }
+                    linearLayout.addView(button)
                 }
-                linearLayout.addView(button)
+                true
             }
-            true
+        } else {
+            return true
         }
-    }
-
-    @UiThread
-    fun getInfo(): String{
-//        val result = ""
-        val result = "{\"news\":[{\"news_id\": 2, \"title\": \"1\", \"content\": \"news1\"}, {\"news_id\": 1, \"title\": \"2\", \"content\": \"news2\"}, {\"news_id\": 3, \"title\": \"3\", \"content\": \"news3\"}]}"
-        Log.i("test result", result)
-//        commServer.setURL(CommServer.NOTIFICATION)
-//        runBlocking {
-//            result = commServer.getInfoBackGroundRunner("UTF-8")
-//            Log.i("GET",result)
-//        }
-        return result
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/hiyama/anpikakuninproject/data/Notification.kt
+++ app/src/main/java/com/hiyama/anpikakuninproject/data/Notification.kt
@@ -1,9 +1,14 @@
 package com.hiyama.anpikakuninproject.data
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.util.Date
 
 class Notification(
-    @JsonProperty("news_id") var news_id : Int,
+    @JsonProperty("author_id") var auth_id : Int,
     @JsonProperty("title") var title: String?,
-    @JsonProperty("content") var content: String?
+    @JsonProperty("content") var content: String?,
+    @JsonProperty("tag") var tag: List<String>?,
+    @JsonProperty("datetime") var datetime: Date?,
+    @JsonProperty("isPublished") var isPublished: Boolean,
+    @JsonProperty("news_id") var news_id: Int
 )
