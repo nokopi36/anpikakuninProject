2d6a5b017fa1269886d170da2161b28187feda5f
RyotaHiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2023-01-26 18:17:02 +0900

    update：URLジャンプボタン追加

--- app/src/main/java/com/hiyama/anpikakuninproject/view/NotificationDialogFragment.kt
+++ app/src/main/java/com/hiyama/anpikakuninproject/view/NotificationDialogFragment.kt
@@ -2,7 +2,11 @@ package com.hiyama.anpikakuninproject.view
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import com.hiyama.anpikakuninproject.R
@@ -17,10 +21,21 @@ class NotificationDialogFragment: DialogFragment() {
             val root = inflater.inflate(R.layout.notification_dialog, null)
             val notificationTitle = root.findViewById<TextView>(R.id.notificationTitle)
             val notificationContent = root.findViewById<TextView>(R.id.notificationContent)
+            val notificationUrl = root.findViewById<Button>(R.id.notificationUrl)
             val title = arguments?.getString("title", "noTitle")
             val content = arguments?.getString("content", "noContent")
+            val url = arguments?.getString("url", "noUrl")
             notificationTitle.text = title
             notificationContent.text = content
+            if (url == "noUrl"){
+                notificationUrl.visibility = View.GONE
+            } else {
+                notificationUrl.text = url
+                notificationUrl.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                    startActivity(intent)
+                }
+            }
 
             //DialogBuilderにdialogのviewをセット
             builder.setView(root)
