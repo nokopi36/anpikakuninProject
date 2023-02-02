e187b8aa4a5174231c08075eea0bb6ebba81ec33
RyotaHiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2023-02-02 19:07:38 +0900

    update：通知権限認証を追加

--- app/src/main/java/com/hiyama/anpikakuninproject/MainActivity.kt
+++ app/src/main/java/com/hiyama/anpikakuninproject/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.hiyama.anpikakuninproject
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -17,12 +24,16 @@ import com.hiyama.anpikakuninproject.activity.PasswordActivity
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ }
+        setUpPermissions()
 
         val toolbar = findViewById<Toolbar>(R.id.toolBar)
         setSupportActionBar(toolbar)
@@ -72,6 +83,22 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setUpPermissions(){
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= 33) {
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                val name = "my-notification-channel"
+                val importance = NotificationManager.IMPORTANCE_DEFAULT
+                val channelId = "${packageName}-$name"
+                val channel = NotificationChannel(channelId, name, importance)
+                channel.description = "my notification channel description"
+                val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(channel)
+            }
         }
     }
 
